74b8ee5bc44a268d4987a97a8a035048fdc38889
Jingchi Ma <34053929+JingchiMa@users.noreply.github.com>
2019-02-26 15:32:37 -0800

    mjc - modified register method in DatabaseService

--- javabackend/src/main/java/database/DatabaseService.java
+++ javabackend/src/main/java/database/DatabaseService.java
@@ -13,6 +13,7 @@ import responses.Response;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 
 public class DatabaseService {
@@ -29,45 +30,57 @@ public class DatabaseService {
         MongoCollection<Document> collection = database.getCollection("device_ips");
         Document document = collection.find(eq("device_id", request.deviceId())).first();
         return document == null ? new ForwardResponse() : new ForwardResponse(document.get("ip").toString());
-        // TODO: why Intellij says document is always not null?
     }
 
     /**
      * Database operation that handles register request.
      * database scheme
-     *     user_to_device table: username, device_id
-     *     devices: device_id, device_ip
+     *     Devices: username, nickname, IP
      * @param request: the RegisterRequest Object
      * @return Response object
      */
     public static Response register(RegisterRequest request) {
         MongoDatabase database = client.getDatabase("Devices");
-        MongoCollection<Document> devices = database.getCollection("devices");
-        MongoCollection<Document> userToDevice = database.getCollection("user_to_device");
+        MongoCollection<Document> devices = database.getCollection("Devices");
 
         List<Document> deviceRecords = new ArrayList<>();
-        List<Document> userToDeviceRecords = new ArrayList<>();
-        String[] deviceIds = new String[request.deviceIps().length];
-
-        int i = 0;
-        for (String deviceIp : request.deviceIps()) {
-            deviceIds[i] = createDeviceId(request.user());
-            deviceRecords.add(new Document("device_id", deviceIds[i]).append("ip", deviceIp));
-            userToDeviceRecords.add(new Document("user", request.user()).append("device_id", deviceIds[i]));
-            i++;
+        RegisterResponse.DeviceRegisterStatus[] statuses = new RegisterResponse.DeviceRegisterStatus[request.deviceIps().length];
+        for (int i = 0; i < request.deviceIps().length; i++) {
+            if (ipInvalid(request.deviceIps()[i])) {
+                statuses[i] = new RegisterResponse.DeviceRegisterFailed("IP already occupied");
+            } else if (deviceNameInvalid(request.user(), request.deviceNames()[i])) {
+                statuses[i] = new RegisterResponse.DeviceRegisterFailed(
+                        "Device name: " + request.deviceNames()[i] + " is already registered under " + request.user()
+                );
+            } else {
+                deviceRecords.add(
+                        new Document("username", request.user())
+                                .append("IP", request.deviceIps()[i])
+                                .append("deviceName", request.deviceNames()[i])
+                );
+                String username = request.user();
+                String deviceName = request.deviceNames()[i];
+                statuses[i] = new RegisterResponse.DeviceRegisterSuccess(createDeviceId(username, deviceName), deviceName);
+                devices.insertMany(deviceRecords);
+            }
         }
-
-        devices.insertMany(deviceRecords);
-        userToDevice.insertMany(userToDeviceRecords);
-
-        return new RegisterResponse(deviceIds);
+        return new RegisterResponse(statuses);
     }
 
-    // TODO: implement method to create device id
-    private static long id = 0;
-    private static String createDeviceId(String username) {
-        id++;
-        return id + "";
+    private static String createDeviceId(String username, String deviceName) {
+        return username + "-" + deviceName;
+    }
+
+    private static boolean ipInvalid(String ip) {
+        MongoDatabase database = client.getDatabase("Devices");
+        MongoCollection<Document> devices = database.getCollection("Devices");
+        return devices.find(eq("IP", ip)).first() != null;
+    }
+
+    private static boolean deviceNameInvalid(String user, String deviceName) {
+        MongoDatabase database = client.getDatabase("Devices");
+        MongoCollection<Document> devices = database.getCollection("Devices");
+        return devices.find(and(eq("username", user), eq("deviceName", deviceName))).first() != null;
     }
 
 }
