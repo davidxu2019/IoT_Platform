92d59c36ce26fec911fb6360541d5608164cb3ae
Jingchi Ma <34053929+JingchiMa@users.noreply.github.com>
2019-03-10 15:40:18 -0700

    xm - changed data forwarding route to send https requst

--- nodefrontend/routes/communication.js
+++ nodefrontend/routes/communication.js
@@ -1,12 +1,12 @@
-// adding library into api router
 let express = require('express');
 let router = express.Router();
-let http = require('http');
+let httpsRequest = require('./exchangePublicKeyRouter/httpsRequest');
 const assert = require('assert');
+let fs = require('fs'); 
 
 router.post('/:username/:deviceID', function(req, res, next) {
     console.log("you have entering dataForwarding api");
-    if(req.body.command == null){
+    if (req.body.command == null) {
         console.log("No embedded command in request");
         res.status(400).json({mes:"No embedded command in request"});
     }
@@ -19,9 +19,11 @@ router.post('/:username/:deviceID', function(req, res, next) {
         let dbo = req.db;
         let query = {$and:[{"username":username},
                 {"deviceID": deviceID}]};
-        dbo.collection("Bindings").find(query).toArray(function(err, device){
+        console.log(username);
+        console.log(deviceID);
+        dbo.collection("Bindings").find(query).toArray(function(err, device) {
             assert.equal(null, err);
-            if(device.length==0){
+            if (device.length == 0) {
                 console.log("Sorry, user doesn't have the authority to interact with this device or this device doesn't exist");
                 res.status(400).json({mes:"Sorry, user doesn't have the authority to interact with this device or this device doesn't exist"});
             }
@@ -32,34 +34,39 @@ router.post('/:username/:deviceID', function(req, res, next) {
                     assert.equal(null, err);
                     let IP = device[0].IP;
                     let port = device[0].port;
-
-                    // setting up http connection, transmit command and wait for response
-                    var post_options = {
-                        host: IP,
-                        port: port,
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'text/plain',
-                            'Content-Length': Buffer.byteLength(command)
-                        }
-                    };
-
-                    // Set up the request
-                    let deviceFeedback = null;
-                    var post_req = http.request(post_options, function(responseFromD) {
-                        responseFromD.setEncoding('utf8');
-                        responseFromD.on('data', function (chunk) {
-                            console.log('Response: ' + chunk);
-                            res.status(200).json(chunk);
-                        });
+                    let postData = JSON.stringify({
+                        "command": command
                     });
-                    // post the data
-                    post_req.write(command);
-                    post_req.end();
+                    forwardRequestForInfo(IP, port, postData)
+                    .then(function(msg) {
+                        res.send(JSON.stringify(msg));
+                    });
                 });
             }
         });
     }
 });
+
+/**
+Helper functions
+*/
+
+function forwardRequestForInfo(IP, port, postData) {
+    let options = { 
+        hostname: IP,
+        port: port, 
+        path: '/mes', 
+        method: 'POST', 
+        key: fs.readFileSync('key.pem'), 
+        cert: fs.readFileSync('cert.pem'), 
+        passphrase: "passphrase",
+        ca: [ fs.readFileSync('system_test/device/cert.pem') ],
+        headers: {
+           'Content-Type': 'application/json',
+           'Content-Length': postData.length
+        },
+    }; 
+    return httpsRequest.sendRequest(options, postData);
+}
 
 module.exports = router;
