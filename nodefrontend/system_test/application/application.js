bf5edd06889ae43a2d439ab5ac453f5489daee7a
Jingchi Ma <34053929+JingchiMa@users.noreply.github.com>
2019-03-11 14:06:50 -0700

    mjc - added test for signup, login, exchangepublickey, and communication routes

--- nodefrontend/system_test/application/application.js
+++ nodefrontend/system_test/application/application.js
@@ -2,40 +2,54 @@ var fs = require('fs');
 var encryptAndDecrypt = require('../encryptAndDecrypt');
 var httpsRequest = require("../httpsRequest");
 
-exchangePublicKey()
-.then(function(msg) {
-    fs.writeFileSync("devicePublicKey.pem", msg.publicKey);
+/** 
+test signup 
+*/
+
+/**
+test login
+*/
+
+
+/**
+test singup, login, exchangePublicKey, and forwarding data
+*/
+
+async function testForwardingData() {
+    let username = 'testUser';
+    let password = 'password';
+    let deviceID = 'deviceID';
+    try {
+        // await signup(username, password);
+        let loginData = await login(username, password);
+        let jwt = loginData.cookies.jwt;
+        let publicKeyData = await exchangePublicKey();
+        fs.writeFileSync("devicePublicKey.pem", publicKeyData.body.publicKey);
+        let data = await sendQuery(jwt, username, deviceID, "GOOD!");
+        return data;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+testForwardingData()
+.then(function(data) {
+    console.log(encryptAndDecrypt.decryptStringWithRsaPrivateKey(data.body.mes, 'key.pem'));
 })
-.then(sendQuery)
-.then(function(msg) {
-    console.log(encryptAndDecrypt.decryptStringWithRsaPrivateKey(msg.mes, 'key.pem'));
-});
 
 /*
 helper functions
 */
-function exchangePublicKey() {
-    let options = { 
-        hostname: 'localhost', 
-        port: 8433, 
-        path: '/exchangePublicKey', 
-        method: 'GET', 
-        key: fs.readFileSync('key.pem'), 
-        cert: fs.readFileSync('cert.pem'), 
-        passphrase: "passphrase",
-        ca: [ fs.readFileSync('../../cert.pem') ]
-    };    
-    return httpsRequest.sendRequest(options);
-}
 
-function sendQuery() {
+function signup(username, password) {
     let postData = JSON.stringify({
-        'command':encryptAndDecrypt.encryptStringWithRsaPublicKey("WOCAO", "devicePublicKey.pem")
+        'username': username,
+        'password': password
     });      
     let options = { 
         hostname: 'localhost',
         port: 8433, 
-        path: '/commu/user1/1user1', 
+        path: '/identity/signup', 
         method: 'POST', 
         key: fs.readFileSync('key.pem'), 
         cert: fs.readFileSync('cert.pem'), 
@@ -44,6 +58,67 @@ function sendQuery() {
         headers: {
           'Content-Type': 'application/json',
           'Content-Length': postData.length
+        }
+    };  
+    return httpsRequest.sendRequest(options, postData);
+}
+
+function login(username, password) {
+    let postData = JSON.stringify({
+        'username': username,
+        'password': password
+    });      
+    let options = { 
+        hostname: 'localhost',
+        port: 8433, 
+        path: '/identity/login', 
+        method: 'POST', 
+        key: fs.readFileSync('key.pem'), 
+        cert: fs.readFileSync('cert.pem'), 
+        passphrase: "passphrase",
+        ca: [ fs.readFileSync('../../cert.pem') ],
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': postData.length
+        }
+    };  
+    return httpsRequest.sendRequest(options, postData);   
+}
+
+function exchangePublicKey(jwt) {
+    let options = { 
+        hostname: 'localhost', 
+        port: 8433, 
+        path: '/exchangePublicKey', 
+        method: 'GET', 
+        key: fs.readFileSync('key.pem'), 
+        cert: fs.readFileSync('cert.pem'), 
+        passphrase: "passphrase",
+        ca: [ fs.readFileSync('../../cert.pem') ],
+        headers: {
+            'Cookie': 'jwt=' + jwt,
+        }
+    };    
+    return httpsRequest.sendRequest(options);
+}
+
+function sendQuery(jwt, username, deviceID, command) {
+    let postData = JSON.stringify({
+        'command':encryptAndDecrypt.encryptStringWithRsaPublicKey(command, "devicePublicKey.pem")
+    });      
+    let options = { 
+        hostname: 'localhost',
+        port: 8433, 
+        path: '/commu/' + username + '/' + deviceID, 
+        method: 'POST', 
+        key: fs.readFileSync('key.pem'), 
+        cert: fs.readFileSync('cert.pem'), 
+        passphrase: "passphrase",
+        ca: [ fs.readFileSync('../../cert.pem') ],
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': postData.length,
+          'Cookie': 'jwt=' + jwt
         }
     };  
     return httpsRequest.sendRequest(options, postData);
