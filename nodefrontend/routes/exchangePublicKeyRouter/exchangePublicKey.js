f3f09c093aa8cdac1e783b256847b2955b91af36
Jingchi Ma <34053929+JingchiMa@users.noreply.github.com>
2019-03-19 21:01:09 -0700

    mjc - implemented getIp in exchangePublicKey route

--- nodefrontend/routes/exchangePublicKeyRouter/exchangePublicKey.js
+++ nodefrontend/routes/exchangePublicKeyRouter/exchangePublicKey.js
@@ -15,19 +15,23 @@ routes
 */
 
 router.get('/', function(req,res) {
-    let deviceId = req.query.id;
-    getPublicKeyInPemPromise(req)
-    .then(function(buffer) {
-        return sendPublicKeyToDevice(buffer, deviceId, req.db);
-    })
-    .then(function(msg) { 
-        res.send(JSON.stringify(msg));
-    });
+    handleRequest(req, res);
 });
 
 /*
 Helper functions
 */
+
+async function handleRequest(req, res) {
+    let deviceId = req.body.deviceID;
+    try {
+        let buffer = await getPublicKeyInPemPromise(req);
+        let msg = await sendPublicKeyToDevice(buffer, deviceId, req.db); // msg is a json object
+        res.json(msg);
+    } catch(err) {
+        res.status(422).json({'error': err.message});
+    }    
+}
 
 function getPublicKeyInPemPromise(req) {
     let rawDer = req.connection.getPeerCertificate().raw;
@@ -35,37 +39,43 @@ function getPublicKeyInPemPromise(req) {
     return func('x509', rawDer, { inform: 'der', outform: 'pem' });
 }
 
-// todo: implement this method
 async function getIp(db, deviceId) {
-    query = {"deviceID": toDeviceID};
-    let devices = await dbo.collection("Devices").find(query).toArray();
+    query = {"deviceID": deviceId};
+    let devices = await db.collection("Devices").find(query).toArray();
     if (devices && devices.length > 0) {
         return devices[0].IP;
+    } else {
+        throw new Error("Not such device exists");
     }
 }
 
-function sendPublicKeyToDevice(buffer, deviceId, db) {
+async function sendPublicKeyToDevice(buffer, deviceId, db) {
     const publicKey = buffer.toString('utf8'); // PEM encoded public key safe to use now
     let postData = JSON.stringify({'msg': publicKey});
-    let options = { 
-        hostname: getIp(db, deviceId),
-        port: 4444, 
-        path: '/exchangePublicKey', 
-        method: 'POST', 
-        key: fs.readFileSync('key.pem'), 
-        cert: fs.readFileSync('cert.pem'), 
-        passphrase: "passphrase",
-        ca: [ fs.readFileSync('system_test/device/cert.pem') ],
-        headers: {
-           'Content-Type': 'application/json',
-           'Content-Length': postData.length
-        },
-        // avoid cache session so that getPublicKeyInPemPromise, may affect performance
-        agent: new https.Agent({ 
-            maxCachedSessions: 0
-        }),
-    }; 
-    return httpsRequest.sendRequest(options, postData);
+    try {
+        let ip = await getIp(db, deviceId);
+        let options = { 
+            hostname: ip,
+            port: 4444, 
+            path: '/exchangePublicKey', 
+            method: 'POST', 
+            key: fs.readFileSync('key.pem'), 
+            cert: fs.readFileSync('cert.pem'), 
+            passphrase: "passphrase",
+            ca: [ fs.readFileSync('system_test/device/cert.pem') ],
+            headers: {
+               'Content-Type': 'application/json',
+               'Content-Length': postData.length
+            },
+            // avoid cache session so that getPublicKeyInPemPromise, may affect performance
+            agent: new https.Agent({ 
+                maxCachedSessions: 0
+            }),
+        }; 
+        return httpsRequest.sendRequest(options, postData);        
+    } catch(err) {
+        throw(err);
+    } 
 }
 
 module.exports = router;
